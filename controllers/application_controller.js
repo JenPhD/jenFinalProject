468dd280806c647a15ef39986ef6c00e03f9b06d
Jennifer Pemberton <jenpem@Admins-MacBook-Pro.local>
2017-01-09 21:03:29 -0500

    fixed user login

--- controllers/application_controller.js
+++ controllers/application_controller.js
@@ -4,7 +4,14 @@ const mongoose = require('mongoose');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('home');
+    res.render('home', {
+      //keep logged_in
+      logged_in: req.session.logged_in,
+      // the username to the session
+      name: req.session.name,
+      // and the user's email.
+      email: req.session.email
+    });
 });
 
 module.exports = router;
